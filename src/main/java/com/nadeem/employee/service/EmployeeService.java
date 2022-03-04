04c62e181832cf92d436337680e67703f9127f3f
Nadeem <nadeemkhadri7867@gmail.com>
2022-03-04 14:51:20 +0530

    initial commit

---
+++ src/main/java/com/nadeem/employee/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.nadeem.employee.service;
+
+import java.util.List;
+
+import com.nadeem.employee.entity.Employee;
+
+public interface EmployeeService {
+	List < Employee > getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+}
